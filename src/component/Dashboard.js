3ef5e2fc3c7d43f4ace1ff8b76e24d6914169ac0
Darshan Kevadiya <darshankevadiya019@gmail.com>
2022-07-14 16:50:45 +0530

    Commit

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './Header'
 import Section1 from './Section1'
+import Section2 from './Section2'
 
 const Dashboard = () => {
   return (
     <>
       <Header />
       <Section1 />
+      <Section2 />
     </>
   )
 }
--- src/component/Section1.js
+++ src/component/Section1.js
@@ -1,12 +1,66 @@
 import React from 'react'
 import Carousel from 'react-bootstrap/Carousel';
-import  bgimage from "../assets/image/Rectangle 43.png";
+import bgimage from "../assets/image/Rectangle 43.png";
+import sloganimg from "../assets/image/Mask group.png";
+import roundarrow from "../assets/image/Group 65.svg";
 
 const Section1 = () => {
   return (
-    <div className="bg-slider">
-<img src={bgimage} />
+    <>
+    <div className='slider-main'>
+    <Carousel>
+    <Carousel.Item interval={3000}>
+      <img
+        className="d-block w-100 bg-image"
+        src={bgimage}
+        alt="First slide"
+      />
+      
+    </Carousel.Item>
+    <Carousel.Item interval={3000}>
+      <img
+        className="d-block w-100 bg-image"
+        src={bgimage}
+        alt="Second slide"
+      />
+      
+    </Carousel.Item>
+    <Carousel.Item>
+      <img
+        className="d-block w-100 bg-image"
+        src={bgimage}
+        alt="Third slide"
+      />
+      
+    </Carousel.Item>
+  </Carousel>
+
+  <div className='slider-content'>
+    <div className='slider-slogan'>
+    <div class="slogan-text">Fresh Mango</div>
+    <div class="slogan-img">
+            <img src={sloganimg} />
+            <div class="slogan-text">IT Support</div>
+          </div>
+          <div class="slogan-text">Services</div>
     </div>
+    <div className='slider-services'>
+    <div class="services">
+      <div>FIND OUT MORE</div>
+      <img src={roundarrow} />
+    </div>
+    <div class="services">
+      <div>CALL FOR IT SUPPORT</div>
+      <img src={roundarrow} />
+    </div>
+    <div class="services">
+      <div>BOOK FREE CUNSULTANCY</div>
+      <img src={roundarrow} />
+    </div>
+    </div>
+  </div>
+  </div>
+  </>
   )
 }
 
