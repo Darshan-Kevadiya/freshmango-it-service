cdfb7909c80483a390ff14b97485d3c7b2458c89
Darshan Kevadiya <darshankevadiya019@gmail.com>
2022-07-18 12:55:20 +0530

    Final Commit(updated)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import './App.css';
-import  SignIn  from './component/SignIn'
-import { BrowserRouter, Routes ,Route} from 'react-router-dom';
+import SignIn from './component/SignIn'
+import {Routes, Route } from 'react-router-dom';
 import SignUp from './component/SignUp';
 import Dashboard from './component/Dashboard';
-import { initializeApp } from "firebase/app";
 import { UserAuthContextProvider } from './context/UserAuthContext';
 import ProtectedRoute from './component/ProtectedRoute';
 
 function App() {
   return (
     <div className="App">
-    <UserAuthContextProvider>
-  <Routes>
-    <Route path="/signin" element={<SignIn />} />
-    <Route path="/signup" element={<SignUp />} />
-    <Route exact path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
-    <Route path="*" element={<SignIn/>} />
-  </Routes>
-  </UserAuthContextProvider>
+      <UserAuthContextProvider>
+        <Routes>
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route exact path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+          <Route path="*" element={<SignIn />} />
+        </Routes>
+      </UserAuthContextProvider>
     </div>
 
   );
